53b02df68950f96d0e24467b5c8bbdb70533a0f2
johannesrose <johannesmarc.rose@docc.techstarter.de>
2023-06-10 16:50:03 +0200

    update

--- website/script.js
+++ website/script.js
@@ -10,7 +10,7 @@ menu.onclick = () => {
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch(
-        "${{ secrets.LAMBDA_URL }}"
+        "https://iib2lyr6v7njzrudnyueamrcuq0ousrq.lambda-url.eu-central-1.on.aws/"
     );
     let data = await response.json();
     counter.innerHTML = `👀 Besucher: ${data}`;
